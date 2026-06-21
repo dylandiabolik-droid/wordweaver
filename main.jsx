8e5e62d9d2067d6b689b97c340c4c9826303d06d
dylandiabolik-droid <dylandiabolik@gmail.com>
2026-06-21 19:54:18 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import EtsyListingGenerator from './EtsyListingGenerator.jsx'
+import { Imprint, Privacy } from './LegalPages.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/etsy-listing-generator" element={<EtsyListingGenerator />} />
+        <Route path="/" element={<EtsyListingGenerator />} />
+        <Route path="/etsy-listing-generator" element={<Navigate to="/" replace />} />
+        <Route path="/imprint" element={<Imprint />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
